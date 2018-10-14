54eaa6d3750a1a1572b0cb0dca78ad182f66589f
mzeeshansikander <zeeshan.sikander@live.com>
2018-10-14 23:26:01 +0500

    albums projects

---
+++ src/components/AlbumList.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+    state={
+        albums:[]
+    };
+
+    componentWillMount(){
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response=>this.setstate({albums: response.data}));
+
+    
+    
+    }
+
+    render(){
+        return ( 
+            <View>
+                <Text>
+                    AlbumList
+                </Text>
+            </View>
+        );
+    }
+};
+
+export default AlbumList;
